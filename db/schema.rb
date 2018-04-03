7f2073e4e76de07c0a989994f3431e73ed479870
Ariel Feingold <feingold.ariel@gmail.com>
2018-04-03 15:39:13 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180403153145) do
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "pasword_digest"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
