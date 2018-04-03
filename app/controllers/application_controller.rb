86d74e9dc38e9a652c0d10884124c7394db0a2af
Ariel Feingold <feingold.ariel@gmail.com>
2018-04-03 16:19:31 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,7 +38,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-    user = User.find_by(:username => params[:username], user.authenticate(params[:password])
+    user = User.find_by(:username => params[:username], :password => user.authenticate(params[:password])
 		if user
 			redirect "/account"
 		else
